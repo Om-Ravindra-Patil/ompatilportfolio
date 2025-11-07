25c01173075d56f6532d1b7e0f853316d196aaf3
Suraj Nate <surajnate29@gmail.com>
2025-11-07 23:22:38 +0530

    Resolve merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<Home />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
