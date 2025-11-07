5b2db92e4c66a02f26d89557c049b1c431605dff
Suraj Nate <surajnate29@gmail.com>
2025-11-08 01:08:35 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 const queryClient = new QueryClient();
 
@@ -15,12 +15,11 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/Home" element={<Home />} />
-            <Route path="*" element={<NotFound />} />  {/* nested 404 */}
-          
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="*" element={<Home />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
